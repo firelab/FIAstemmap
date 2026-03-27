e1bd1bf927bc99601645956c62c86e5357806ad1
Chris Toney <jctoney@gmail.com>
2026-03-27 14:09:31 -0600

    update height metric expect values

--- tests/testthat/test-calc_ht_metrics.R
+++ tests/testthat/test-calc_ht_metrics.R
@@ -6,14 +6,14 @@ test_that("calc_ht_metrics works", {
                          "maxSapHt")
 
     expected$numTrees <- 89
-    expected$meanTreeHt <- 45
-    expected$meanTreeHtBAW <- 45.4
-    expected$meanTreeHtDom <- 45
-    expected$meanTreeHtDomBAW <- 45.4
+    expected$meanTreeHt <- 44.8
+    expected$meanTreeHtBAW <- 45.3
+    expected$meanTreeHtDom <- 44.8
+    expected$meanTreeHtDomBAW <- 45.3
     expected$maxTreeHt <- 51
-    expected$predomTreeHt <- 50.3
+    expected$predomTreeHt <- 51
     expected$numSaplings <- 2
-    expected$meanSapHt <- 33.5
-    expected$maxSapHt <- 42
+    expected$meanSapHt <- 34.5
+    expected$maxSapHt <- 43
     expect_equal(calc_ht_metrics(plantation), expected, tolerance = 0.1)
 })
