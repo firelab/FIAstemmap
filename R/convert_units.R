20a45fdad581e3381d49718470983ca8aeb833cb
Chris Toney <jctoney@gmail.com>
2026-04-07 17:01:22 -0600

    add ac_to_ha() and ha_to_ac()

--- R/convert_units.R
+++ R/convert_units.R
@@ -1,7 +1,7 @@
-#' Convenience functions for length unit conversion
+#' Convenience functions for common unit conversions
 #'
 #' Functions to convert between US customary units and SI units for
-#' measurements of length used with tree data.
+#' measurements of length and area commonly used with tree data.
 #'
 #' @name convert_units
 #' @details
@@ -13,9 +13,17 @@
 #'
 #' `cm_to_in(x)` converts centimeters to inches.
 #'
+#' `ac_to_ha()` converts acres to hectares.
+#'
+#' `ha_to_ac()` converts hectares to acres.
+#'
 #' @param x Numeric vector of values to convert.
 #' @return
 #' A numeric vector of the converted values.
+#'
+#' @note
+#' The hectare (ha) is technically a non-SI unit of area that is [accepted for
+#' use with SI](https://en.wikipedia.org/wiki/International_System_of_Units#Non-SI_units_accepted_for_use_with_SI).
 #'
 #' @examples
 #' ft_to_m(1)
@@ -25,6 +33,10 @@
 #' in_to_cm(1)
 #'
 #' cm_to_in(1)
+#'
+#' ac_to_ha(1)
+#'
+#' ha_to_ac(1)
 #' @export
 ft_to_m <- function(x) {
     if (!is.numeric(x)) {
@@ -70,4 +82,28 @@ cm_to_in <- function(x) {
     }
 
     x * 0.393701
+}
+
+#' @name convert_units
+#' @export
+ac_to_ha <- function(x) {
+    if (!is.numeric(x)) {
+        stop(cli::format_error(c(
+            "{.var x} must be a numeric vector",
+        "x" = "Invalid input type: {.cls {class(x)}}")))
+    }
+
+    x * 4046.8564224 / 10000
+}
+
+#' @name convert_units
+#' @export
+ha_to_ac <- function(x) {
+    if (!is.numeric(x)) {
+        stop(cli::format_error(c(
+            "{.var x} must be a numeric vector",
+        "x" = "Invalid input type: {.cls {class(x)}}")))
+    }
+
+    x * 10000 / 4046.8564224
 }
--- tests/testthat/test-convert_units.R
+++ tests/testthat/test-convert_units.R
@@ -11,6 +11,10 @@ test_that("convert_units works", {
     expect_equal(cm_to_in(1), 0.393701)
     expect_equal(cm_to_in(c(1, 10)), c(0.393701, 3.93701))
 
+    expect_equal(ac_to_ha(1), 0.404685642, tolerance = 1e-6)
+    expect_equal(ac_to_ha(c(1, 10)), c(0.404685642, 4.04685642),
+                 tolerance = 1e-6)
+
     x_ft <- 24
     x_m <- ft_to_m(x_ft)
     expect_equal(m_to_ft(x_m), x_ft, tolerance = 1e-4)
@@ -19,9 +23,15 @@ test_that("convert_units works", {
     x_cm <- in_to_cm(x_in)
     expect_equal(cm_to_in(x_cm), x_in, tolerance = 1e-4)
 
-    x <- "char"
+    x_ac <- 6.018046
+    x_ha <- ac_to_ha(x_ac)
+    expect_equal(ha_to_ac(x_ha), x_ac, tolerance = 1e-4)
+
+    x <- "non-numeric"
     expect_error(ft_to_m(x), "numeric vector")
     expect_error(m_to_ft(x), "numeric vector")
     expect_error(in_to_cm(x), "numeric vector")
     expect_error(cm_to_in(x), "numeric vector")
+    expect_error(ac_to_ha(x), "numeric vector")
+    expect_error(ha_to_ac(x), "numeric vector")
 })
