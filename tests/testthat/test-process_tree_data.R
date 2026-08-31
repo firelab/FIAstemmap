0f90cb837db9f50fa4177884260951f16c7ffd31
Chris Toney <jctoney@gmail.com>
2026-08-31 11:00:15 -0600

    suppress cascaded warning from spatstat

--- tests/testthat/test-process_tree_data.R
+++ tests/testthat/test-process_tree_data.R
@@ -1,8 +1,10 @@
 test_that("process_tree_data works", {
     f <- system.file("extdata/test-tree_data.csv", package="FIAstemmap")
     tree_in <- load_tree_data(f)
-    # warning due to points outside window, from PNW macroplot trees
-    expect_warning(out <- process_tree_data(tree_in))
+    # spatstat warning due to points outside window, from PNW macroplot trees
+    suppressWarnings({
+        expect_warning(out <- process_tree_data(tree_in))
+    })
     expect_true(is.data.frame(out))
     expect_equal(out$model_tcc[1:4], c(95.0, 71.4, 70.1, 25.3), tolerance = 1e-1)
 })
